dfac77612ad357cf6893f43282540c4c6bec0c63
HoangVu <hoangvu1991uit@gmail.com>
2020-03-27 15:20:13 +0700

    [Java Thread Basics] CompletionService Interface

---
+++ src/JavaThreadBasics/CompletionServiceInterface/Demonstration.java
@@ -0,0 +1,83 @@
+package JavaThreadBasics.CompletionServiceInterface;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class Demonstration {
+
+    static Random random = new Random(System.currentTimeMillis());
+
+    public static void main(String args[]) throws Exception {
+        completionServiceExample();
+        /*
+        0
+        1
+        Thread0 got done.
+        Thread1 got done.
+        16
+        4
+        Thread4 got done.
+        Thread2 got done.
+        9
+        Thread3 got done.
+        25
+        36
+        Thread5 got done.
+        Thread6 got done.
+        64
+        Thread8 got done.
+        49
+        Thread7 got done.
+        81
+        Thread9 got done.
+        */
+    }
+
+
+    static void completionServiceExample() throws Exception {
+
+        class TrivialTask implements Runnable {
+
+            int n;
+
+            public TrivialTask(int n) {
+                this.n = n;
+            }
+
+            public void run() {
+                try {
+                    // sleep for one second
+                    Thread.sleep(random.nextInt(101));
+                    System.out.println(n * n);
+                } catch (InterruptedException ie) {
+                    // swallow exception
+                }
+            }
+        }
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        ExecutorCompletionService<Integer> service =
+                new ExecutorCompletionService<Integer>(threadPool);
+
+        // Submit 10 trivial tasks.
+        for (int i = 0; i < 10; i++) {
+            service.submit(new TrivialTask(i), new Integer(i));
+        }
+
+        // wait for all tasks to get done
+        int count = 10;
+        while (count != 0) {
+            Future<Integer> f = service.poll();
+            if (f != null) {
+                System.out.println("Thread" + f.get() + " got done.");
+                count--;
+            }
+        }
+
+        threadPool.shutdown();
+    }
+
+}

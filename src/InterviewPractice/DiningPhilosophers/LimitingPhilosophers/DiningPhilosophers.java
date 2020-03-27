7502f8535330025b7132947ead151a551a0a8101
HoangVu <hoangvu1991uit@gmail.com>
2020-03-27 11:31:41 +0700

    [I Practice Problems] Dining Philosophers - the naive solution

--- src/InterviewPractice/DiningPhilosophers/LimitingPhilosophers/DiningPhilosophers.java
+++ src/InterviewPractice/DiningPhilosophers/LimitingPhilosophers/DiningPhilosophers.java
@@ -1,15 +1,16 @@
-package InterviewPractice.DiningPhilosophers.Sol;
+package InterviewPractice.DiningPhilosophers.LimitingPhilosophers;
 
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class DiningPhilosophers {
 
+    // This random variable is used for test purporses only
     private static Random random = new Random(System.currentTimeMillis());
-
+    // Five semaphore represent the five forks.
     private Semaphore[] forks = new Semaphore[5];
-    private Semaphore maxDiners = new Semaphore(4);
 
+    // Initializing the semaphores with a permit of 1
     public DiningPhilosophers() {
         forks[0] = new Semaphore(1);
         forks[1] = new Semaphore(1);
@@ -18,6 +19,7 @@ public class DiningPhilosophers {
         forks[4] = new Semaphore(1);
     }
 
+    // Represents how a philosopher lives his life
     public void lifecycleOfPhilosopher(int id) throws InterruptedException {
 
         while (true) {
@@ -26,24 +28,13 @@ public class DiningPhilosophers {
         }
     }
 
+    // We can sleep the thread when the philosopher is thinking
     void contemplate() throws InterruptedException {
         Thread.sleep(random.nextInt(500));
     }
 
+    // This method will have the meat of the solution, where the
+    // philosopher is trying to eat.
     void eat(int id) throws InterruptedException {
-
-        // acquire the left fork first
-        forks[id].acquire();
-
-        // acquire the right fork second
-        forks[(id + 1) % 5].acquire();
-
-        // eat to your heart's content
-        System.out.println("Philosopher " + id + " is eating");
-
-        // release forks for others to use
-        forks[id].release();
-        forks[(id + 1) % 5].release();
-
     }
 }

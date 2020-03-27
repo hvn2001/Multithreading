df6c572563ceb1b092184e06d2686704b176a9ea
HoangVu <hoangvu1991uit@gmail.com>
2020-03-27 11:02:59 +0700

    [I Practice Problems] Implementing a Barrier - Final Cut

---
+++ src/InterviewPractice/ImplementingBarrier/FinalCut/Test/Barrier.java
@@ -0,0 +1,41 @@
+package InterviewPractice.ImplementingBarrier.FinalCut.Test;
+
+public class Barrier {
+    int released = 0;
+    int count = 0;
+    int totalThreads;
+
+    public Barrier(int totalThreads) {
+        this.totalThreads = totalThreads;
+    }
+
+    public synchronized void await() throws InterruptedException {
+
+        // block any new threads from proceeding till,
+        // all threads from previous barrier are released
+        while (count == totalThreads) wait();
+
+        // increment the counter whenever a thread arrives at the
+        // barrier.
+        count++;
+
+        if (count == totalThreads) {
+            // wake up all the threds.
+            notifyAll();
+            // remember to set released to totalThreads
+            released = totalThreads;
+        } else {
+            // wait till all threads reach barrier
+            while (count < totalThreads)
+                wait();
+        }
+
+        released--;
+        if (released == 0) {
+            count = 0;
+            // remember to wakeup any threads
+            // waiting on line#14
+            notifyAll();
+        }
+    }
+}

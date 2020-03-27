a9e9ccd36afebc30273a2e373ed1e767ad2e8dab
HoangVu <hoangvu1991uit@gmail.com>
2020-03-27 11:47:34 +0700

    [I Practice Problems] Barber Shop - Init

---
+++ src/InterviewPractice/BarberShop/Initial/BarberShopProblem.java
@@ -0,0 +1,17 @@
+package InterviewPractice.BarberShop.Initial;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BarberShopProblem {
+
+    final int CHAIRS = 3;
+    int waitingCustomers = 0;
+    ReentrantLock lock = new ReentrantLock();
+
+    void customerWalksIn() throws InterruptedException {
+
+    }
+
+    void barber() throws InterruptedException {
+    }
+}

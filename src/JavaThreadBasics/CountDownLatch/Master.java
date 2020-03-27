1b5efd1e8bb5b482507f46c62a011ff4a6bed226
HoangVu <hoangvu1991uit@gmail.com>
2020-03-27 15:25:28 +0700

    [Java Thread Basics] CountDownLatch

---
+++ src/JavaThreadBasics/CountDownLatch/Master.java
@@ -0,0 +1,18 @@
+package JavaThreadBasics.CountDownLatch;
+
+public class Master extends Thread {
+    public Master(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Master executed " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
---
+++ src/JavaThreadBasics/CountDownLatch/Worker.java
@@ -0,0 +1,29 @@
+package JavaThreadBasics.CountDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * The manufacturer thread that has to complete its tasks first
+ */
+public class Worker extends Thread {
+    private CountDownLatch countDownLatch;
+
+    public Worker(CountDownLatch countDownLatch, String name) {
+        super(name);
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Worker " + Thread.currentThread().getName() + " started");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Worker  " + Thread.currentThread().getName() + " finished");
+
+        //Each thread calls countDown() method on task completion.
+        countDownLatch.countDown();
+    }
+}

0a5588be3ed9f7b961444fe4abfffb39c7e9032b
HoangVu <hoangvu1991uit@gmail.com>
2020-03-27 11:50:59 +0700

    [I Practice Problems] Barber Shop - F1

--- src/InterviewPractice/BarberShop/F2/BarberShopProblem.java
+++ src/InterviewPractice/BarberShop/F2/BarberShopProblem.java
@@ -1,5 +1,6 @@
 package InterviewPractice.BarberShop.F2;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BarberShopProblem {
@@ -7,9 +8,25 @@ public class BarberShopProblem {
     final int CHAIRS = 3;
     int waitingCustomers = 0;
     ReentrantLock lock = new ReentrantLock();
+    Semaphore waitForCustomerToEnter = new Semaphore(0);
+    Semaphore waitForBarberToGetReady = new Semaphore(0);
 
     void customerWalksIn() throws InterruptedException {
+        lock.lock();
+        if (waitingCustomers == CHAIRS) {
+            System.out.println("Customer walks out, all chairs occupied.");
+            // Remember to unlock before leaving
+            lock.unlock();
+            return;
+        }
+        waitingCustomers++;
+        lock.unlock();
 
+        // Let the barber know you are here, in case he's asleep
+        waitForCustomerToEnter.release();
+        // Wait for the barber to come take you to the salon chair when its your turn
+        waitForBarberToGetReady.acquire();
+        // TODO: complete the rest of the logic.
     }
 
     void barber() throws InterruptedException {
